9efabd17da5b767e8c06871f4a3337991d648cce
xiexingen <1002275364@qq.com>
2018-09-27 19:37:42 +0800

    修改plugins.js文件中的theme_css不适用cdn文件

--- themes/indigo/scripts/plugins.js
+++ themes/indigo/scripts/plugins.js
@@ -11,7 +11,7 @@ const source = (path, cache, ext) => {
     }
 }
 hexo.extend.helper.register('theme_js', (path, cache) => source(path, cache, '.js'))
-hexo.extend.helper.register('theme_css', (path, cache) => source(path, cache, '.css'))
+hexo.extend.helper.register('theme_css', (path, cache) => source(path, false, '.css'))
 
 function renderImage(src, alt = '', title = '') {
     return `<figure class="image-bubble">
